2b17cc70610e555fb386d6b81c2abdc9e07e45a4
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-08 13:36:44 +0530

    Create Even_or_Odd.py

---
+++ Basic_programs/Even_or_Odd.py
@@ -0,0 +1,18 @@
+x=input("Enter a number")
+x=int(x);
+if(x%2==0):
+    print("Given Number is even")
+else:
+    print("Given number is odd")
+
+
+Result:
+
+Enter a number34
+Given Number is even
+
+Result:
+
+Enter a number23
+Given number is odd
+
