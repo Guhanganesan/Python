4022985fa96b8356753094260b567e43430f2809
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-08 09:55:05 +0530

    Create Greatest_among_three_numbers.py

---
+++ Basic_programs/Greatest_among_three_numbers.py
@@ -0,0 +1,16 @@
+x=input("Enter First Number")
+y=input("Enter Second Number")
+z=input("Enter third Number")
+if(x>=y and x>=z):
+    print(x + "is largest")
+elif(y>=x and y>=z):
+    print(y+"is largest")
+else:
+    print(z+"is largest")
+    
+Result:
+
+  Enter First Number14
+  Enter Second Number12
+  Enter third Number10
+  14is largest
