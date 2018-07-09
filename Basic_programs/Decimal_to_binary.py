8541b43b0a5852062066e459e7a91d38a9cc1986
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-09 13:31:20 +0530

    Create Decimal_to_binary.py

---
+++ Basic_programs/Decimal_to_binary.py
@@ -0,0 +1,18 @@
+num=14
+a=0
+b=1
+while num>=1:
+      if num%2==0:
+          print(a)
+      elif num%2==1:
+          print(b)
+      num = num // 2
+      
+      
+      
+  Result:
+  
+  0
+  1
+  1
+  1
