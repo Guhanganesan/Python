cb42904239f215058c182d8dad94e2265dbc828b
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-11 13:02:28 +0530

    Create Fibonacci_series.py

---
+++ Basic_programs/Fibonacci_series.py
@@ -0,0 +1,23 @@
+first=0
+second=1
+next=0
+i=0;
+while(i<=8):
+       print(first)
+       next=first+second
+       first=second
+       second=next
+       i=i+1
+       
+       
+Result:
+
+0
+1
+1
+2
+3
+5
+8
+13
+21
