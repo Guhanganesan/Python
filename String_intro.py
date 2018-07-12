b3b78551891bc61ea4ef6b31d4b59c72158c4f9f
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-12 12:53:57 +0530

    Create String_intro.py

---
+++ String_intro.py
@@ -0,0 +1,20 @@
+name="Guhan Ganesan"
+msg='Hi guhan how are you'
+
+email =""" Subject: Regarding..
+           I am going to attend on interview....
+"""
+
+addr='''  G. Guhan
+          Velachery
+          Chennai
+          600042
+'''
+
+print(name)
+
+print(msg)
+
+print(email)
+
+print(addr)
