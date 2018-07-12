dd62314209da45eeb35edb9b5f310c05ae644dae
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-12 16:37:00 +0530

    Create String_method1.py

---
+++ String_method1.py
@@ -0,0 +1,46 @@
+name="Guhan"
+print("guhan" "Ganesan")
+print('x\97\x98')
+print(name[::-1])#nahuG
+x=len(name)
+print(x)
+for i in range(0,x):
+    print(name[i])
+
+i=0
+while i<x:
+    print(name[i])
+    i=i+1
+
+for m in "ABCD":
+    print(m)
+
+name="Alagesana"
+count=0
+for char in name:
+     if char=="a":
+        count=count+1
+print("The number od a is:",count)
+
+
+Result:
+
+    guhanGanesan
+x\97
+nahuG
+5
+G
+u
+h
+a
+n
+G
+u
+h
+a
+n
+A
+B
+C
+D
+The number od a is: 3
