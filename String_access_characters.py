5a3d1ceb8d63685b37a2f03970a0236704b184b4
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-12 13:05:11 +0530

    Create String_access_characters.py

---
+++ String_access_characters.py
@@ -0,0 +1,26 @@
+name="Guhan Ganesan"
+print(name)
+print(name[0])
+print(name[-1])
+print(name[-3])
+print(name[-3:])
+print(name[:-3])
+print(name[2:])
+print(name[:2])
+print(name[1:6])
+print(name[-3:-1])
+
+
+Result:
+
+Guhan Ganesan
+G
+n
+s
+san
+Guhan Gane
+han Ganesan
+Gu
+uhan 
+sa
+
