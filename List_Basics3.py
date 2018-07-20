62dde726d692ab361ac8ecd8e7f3816d694a8835
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-20 07:45:38 +0530

    Rename List_Basics2.py to List_Basics3.py

---
+++ List_Basics3.py
@@ -0,0 +1,23 @@
+#Create List
+value=0
+L=[]
+while value>=0:
+    x = int(input("Enter values"))
+    L=L+[x]
+    print(L)
+    
+ Result:
+    Enter values10
+    [10]
+    Enter values12
+    [10, 12]
+    Enter values13
+    [10, 12, 13]
+    Enter values15
+    [10, 12, 13, 15]
+    Enter values
+    
+    
+    
+    
+    
