427f4c1be3dcfe04b9ccdfa63165684af9de9909
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-20 07:49:46 +0530

    Update List_Basics3.py

--- List_Basics3.py
+++ List_Basics3.py
@@ -17,6 +17,29 @@ while value>=0:
     [10, 12, 13, 15]
     Enter values
     
+  #Using function
+
+    def cal():
+        value=0
+        L=[]
+        while value>=0:
+            x = int(input("Enter values"))
+            L=L+[x]
+            print(L)
+    cal()
+    
+    Result:
+        Enter values12
+        [12]
+        Enter values13
+        [12, 13]
+        Enter values
+        
+        
+        
+        
+
+    
     
     
     
