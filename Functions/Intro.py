e92dce8871e0194fc135292bece393ed66df1019
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-21 15:21:41 +0530

    Create Intro.py

---
+++ Functions/Intro.py
@@ -0,0 +1,33 @@
+#Functions
+
+def test():
+    print("Hi")
+test()
+
+name="Guhan"
+
+def test1():
+    print(name)
+test1();
+
+x=0
+def test2(x):
+    print(x)
+test2(x)
+
+y=0
+def test3(x,y):
+    print(x+y)
+test3(10,20)
+
+def test4(x,y):
+    return  x+y
+m=test4(10,20)
+print(m)
+
+Result:
+Hi
+Guhan
+0
+30
+30
