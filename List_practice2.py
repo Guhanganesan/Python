cdec1df39fffcf04f06c75b66086bc81bb7f6089
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-28 07:53:52 +0530

    Create List_practice2.py

---
+++ List_practice2.py
@@ -0,0 +1,16 @@
+# Interchange a given string
+name="Raghava Guhan"
+name = name.split();
+print(name)
+name.reverse()
+print(name)
+name=" ".join(name)
+print(name)
+
+
+Result:
+
+  ['Raghava', 'Guhan']
+  ['Guhan', 'Raghava']
+  Guhan Raghava
+
