d552c5b2522cff6059a0ffabfe6701b4819ac310
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-28 10:09:48 +0530

    Create String_practice1.py

---
+++ String_practice1.py
@@ -0,0 +1,11 @@
+# String remove white space and combine all words
+name="Raghava  Guhan"
+name = name.strip()
+print(name)
+name=name.replace(" ","")
+print(name)
+
+Result:
+Raghava  Guhan
+RaghavaGuhan
+
