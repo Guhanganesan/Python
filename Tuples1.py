216d249d7aa07e076b02e2164bd10737e9273682
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-07-30 14:04:18 +0530

    Create Tuples1.py

---
+++ Tuples1.py
@@ -0,0 +1,42 @@
+t1 = (1,2,3)
+t2=(4,5,6)
+t=t1+t2
+print(len(t))
+print(t)
+# t[0]=200
+# print(t) Error
+# del t[0] does not support
+print(any(t1))
+print(enumerate(t1))
+print(list(enumerate(t1)))
+
+for i in enumerate(t1):
+    print(i)
+
+for i, j in enumerate(t1):
+    print(i, j)
+
+for i, j in enumerate(t1, 5):
+    print(i, j)
+
+t1=(2,5,3,1,4)
+print(sorted(t1))
+
+Result:
+    6
+    (1, 2, 3, 4, 5, 6)
+    True
+    <enumerate object at 0x000000CE04C19C60>
+    [(0, 1), (1, 2), (2, 3)]
+    (0, 1)
+    (1, 2)
+    (2, 3)
+    0 1
+    1 2
+    2 3
+    5 1
+    6 2
+    7 3
+    [1, 2, 3, 4, 5
+
+
