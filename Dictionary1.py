cde714ede21142fb9a4f619e90ee096ecc4ef7bf
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-01 19:31:17 +0530

    Update Dictionary1.py

--- Dictionary1.py
+++ Dictionary1.py
@@ -10,16 +10,34 @@ d.update(d1)
 print(d)
 d['name']="Tera" # update existing value
 print(d)
+d['xyzz']=122
+print(d)
+print('name'in d)
 
+for i in d:
+    if 'name' in d:
+        print("yes")
+        break
+    else:
+        print("No")
 
+for key, value in d.items():
+    print(key,"=>",value)
 
 Result:
 
+anbu
 anbu
 866
 {'name': 'anbu', 'age': 25}
 {'name': 'anbu'}
 {'name': 'anbu', 'mob': 67587}
 {'name': 'Tera', 'mob': 67587}
+{'name': 'Tera', 'mob': 67587, 'xyzz': 122}
+True
+yes
+name => Tera
+mob => 67587
+xyzz => 122
 
 
