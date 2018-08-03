4884e78af18e3cce1a1e95d5ef1738ad3230f5a6
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-03 16:59:26 +0530

    Update File.py

--- File.py
+++ File.py
@@ -1,13 +1,18 @@
 import os
-f = open("C:\\test\\myfile.txt", "r+")
-#str = f.read(10); # only for 10 characters
-#print(str);
+filepath = 'C:\\test\\myfile.txt'
+file_open= open(filepath,'r+')
+#file_read=file_open.read() #reads all text
+#print(file_read)
+
+#file_read1=file_open.read(10)
+#print(file_read1)
 
 L=[]
-for line in f:
-      # print(line)
-      L.append(line)
+for line in file_open:
+    L.append(line)
 print(L)
+
+
 
 s = os.path.getsize("C:\\test\\myfile.txt")
 print(s)
