59ab9b6fa4eebabef3e263caf050d2a1e00263eb
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-03 17:04:30 +0530

    Update File_write.py

--- File_write.py
+++ File_write.py
@@ -4,4 +4,18 @@ f.close()
 
 Result:
        Hi Guhan How are you
+      
+filepath = 'C:\\test\\myfile.txt'
+file_open= open(filepath,'r+')
+file_open.write("Hi dear guhan\n"
+                "How are you\n"
+                "Where are you\n")
+
+Result in myfile.txt:
+Hi dear guhan
+How are you
+Where are you
+       
+
+       
        
