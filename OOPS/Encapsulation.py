8ce4236cb91d2106a85684a067cd7364ce8f05b6
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-07 17:21:34 +0530

    Create Encapsulation.py

---
+++ OOPS/Encapsulation.py
@@ -0,0 +1,17 @@
+class Training:
+  def __init__(self, name, friend, age):
+       self._public=name
+       self._protected=friend
+       self._private=age
+  def getValues(self):
+      print(self._public)
+      print(self._protected)
+      print(self._private)
+obj = Training("Muthu","Guhan",32)
+obj.getValues()
+
+Result:-
+
+Muthu
+Guhan
+32
