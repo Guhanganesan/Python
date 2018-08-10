b57c30e9ec40cbedb3fe14bb54f5b4a691812ecd
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-10 07:46:04 +0530

    Create While_continue_break.py

---
+++ Basic_programs/While_continue_break.py
@@ -0,0 +1,26 @@
+i=10
+while i>=1:
+    print(i)
+    i = i - 1
+    if i==5:
+        #continue
+        break
+
+Result for break:
+10
+9
+8
+7
+6
+
+Result for continue:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
