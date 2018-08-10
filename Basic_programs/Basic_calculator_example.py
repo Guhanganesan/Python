96c52a6091dd42ee30b11f28eb01a051a1a10784
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-10 08:00:35 +0530

    Create Basic_calculator_example.py

---
+++ Basic_programs/Basic_calculator_example.py
@@ -0,0 +1,34 @@
+n1=int(input("Enter 1st Number"))
+n2=int(input("Enter 2nd Number"))
+x=input("Enter operation choice(+/-/%/*)")
+if x=="+":
+    print(n1+n2)
+elif x=="-":
+    print(n1-n2)
+elif x=="%":
+    print(n1/n2)
+elif x=="*":
+    print(n1*n2)
+  
+  
+Result:
+
+Enter 1st Number22
+Enter 2nd Number34
+Enter operation choice(+/-/%/*)+
+56
+
+Enter 1st Number45
+Enter 2nd Number46
+Enter operation choice(+/-/%/*)-
+-1
+
+Enter 1st Number20
+Enter 2nd Number2
+Enter operation choice(+/-/%/*)%
+10.0
+
+Enter 1st Number20
+Enter 2nd Number2
+Enter operation choice(+/-/%/*)*
+40
