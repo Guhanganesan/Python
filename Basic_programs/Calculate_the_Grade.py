27a0523532a81fd35d68c62b1052b87d2e4c8016
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-10 07:51:19 +0530

    Create Calculate_the_Grade.py

---
+++ Basic_programs/Calculate_the_Grade.py
@@ -0,0 +1,24 @@
+x=int(input("Enter any Number"))
+if x>=90:
+    print("Grade A")
+elif x>=80:
+    print("Grade B")
+elif x>=70:
+    print("Grade C")
+elif x>=60:
+    print("Grade D")
+elif x>=45:
+    print("Grade E")
+else:
+    print("Re Appear")
+    
+    
+ Result:
+Enter any Number88
+Grade B
+
+Enter any Number44
+Re Appear
+
+Enter any Number56
+Grade E
