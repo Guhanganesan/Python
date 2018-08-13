72cb20f75555930a8fa660e171df5e93921b4ab7
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-13 07:22:55 +0530

    Update String_intro.py

--- String_intro.py
+++ String_intro.py
@@ -1,3 +1,7 @@
+#String Definition. In computer science a string is any finite sequence of characters (i.e., letters, numerals,
+#symbols and punctuation marks). An important characteristic of each string is its length, which is the number of characters in it. 
+#The length can be any natural number (i.e., zero or any positive integer).
+
 name="Guhan Ganesan"
 msg='Hi guhan how are you'
 
