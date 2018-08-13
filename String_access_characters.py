fc4d326d7a68d038e0db9b2017ef0302aa52a878
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-13 07:31:24 +0530

    Update String_access_characters.py

--- String_access_characters.py
+++ String_access_characters.py
@@ -3,8 +3,8 @@ print(name)
 print(name[0])
 print(name[-1])
 print(name[-3])
-print(name[-3:])
-print(name[:-3])
+print(name[-3:]) # from -3
+print(name[:-3]) # from -4
 print(name[2:])
 print(name[:2])
 print(name[1:6])
