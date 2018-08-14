eccf6eaefe29d6bd8d47d9e93c25e6e501377097
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-14 15:57:03 +0530

    Update Encapsulation.py

--- OOPS/Encapsulation.py
+++ OOPS/Encapsulation.py
@@ -1,4 +1,3 @@
-#Encapsulation
 class Training:
   def __init__(self, name, friend, age):
        self._public=name
@@ -9,10 +8,23 @@ class Training:
       print(self._protected)
       print(self._private)
 obj = Training("Muthu","Guhan",32)
-obj.getValues()
+#obj.getValues()
+print(obj._private)
+
+class Branch(Training):
+       def check(self):
+           print(self._public)
+           print(self._protected)
+           print(self._private)
+obj1 = Branch(2,3,4)
+#print(obj1.getValues())
+obj1.check()
+print(obj1._private)
 
 Result:-
 
-Muthu
-Guhan
 32
+2
+3
+4
+4
