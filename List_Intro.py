0e278a1b7f882b1cda171ed3714e0b05a4a1f73d
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-16 07:54:21 +0530

    Update List_Intro.py

--- List_Intro.py
+++ List_Intro.py
@@ -1,63 +1,61 @@
 #List is a collection which is ordered and changeable. Allows duplicate members.
 #list() constructor to make a list
 
-books= list(("Tamil","English","Maths"))
-print(books)
 
-books=['Tamil', 'English', 'Maths']
-books.append("Science")
-print(books)
-
-books.reverse()
-print(books)
-
-print(books.count("Tamil"))
-
-x=books.copy()
-print(x)
-
-m=x.clear()
-print(m)
-
-m=books.pop()
-print(m)
-
-print(books)
-
-m=books.pop(1)
-print(m)
-
-print(books)
-
-books.insert(1,"Chemaistry")
-print(books)
-
-books.extend(["History","Physics"])
-print(books)
-
-books.sort()
-
-print(books)
-
-
-print(books.__contains__("Physics"))
+list=["Tamil","English","Maths"]
+print(len(list))
+list.pop()
+print(list)
+list.append("Maths")
+print(list)
+list.pop(1)
+print(list)
+list.insert(1,"English")
+print(list)
+del(list[0])
+print(list)
+list.extend(["Science","Social","Physics"])
+print(list)
+del(list[2])
+print(list)
+list.remove("Maths")
+print(list)
+list.reverse()
+print(list)
+print(list.count("Social"))
+copy=list.copy()
+print(copy)
+clear=copy.clear()
+print(clear)
+list.sort()
+print(list)
+print(list.__contains__("Physics"))
+list.__delitem__(0)
+print(list)
+item=list.__getitem__(0)
+print(item)
 
 Result:-
-
+    
+	3
+['Tamil', 'English']
 ['Tamil', 'English', 'Maths']
-['Tamil', 'English', 'Maths', 'Science']
-['Science', 'Maths', 'English', 'Tamil']
+['Tamil', 'Maths']
+['Tamil', 'English', 'Maths']
+['English', 'Maths']
+['English', 'Maths', 'Science', 'Social', 'Physics']
+['English', 'Maths', 'Social', 'Physics']
+['English', 'Social', 'Physics']
+['Physics', 'Social', 'English']
 1
-['Science', 'Maths', 'English', 'Tamil']
+['Physics', 'Social', 'English']
 None
-Tamil
-['Science', 'Maths', 'English']
-Maths
-['Science', 'English']
-['Science', 'Chemaistry', 'English']
-['Science', 'Chemaistry', 'English', 'History', 'Physics']
-['Chemaistry', 'English', 'History', 'Physics', 'Science']
+['English', 'Physics', 'Social']
 True
+['Physics', 'Social']
+Physics
+
+
 
 
 
