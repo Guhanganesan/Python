461d19f90ebaa79c4a0ae77e3870715c3d05b33a
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-16 10:46:00 +0530

    Update List_practice2.py

--- List_practice2.py
+++ List_practice2.py
@@ -14,3 +14,32 @@ Result:
   ['Guhan', 'Raghava']
   Guhan Raghava
 
+list=[]
+sum=0
+for i in range(0,3):
+    for j in range(0,3):
+        list.append((i+j)**2)
+print(list)
+
+#Ans:[0, 1, 4, 1, 4, 9, 4, 9, 16]
+
+list=[[2,5,7,8],[2,3,4,5],[4,3,2,1]]
+sum=0
+for i in range(0,3):
+    for j in range(0,3):
+        sum=sum+list[i][j]
+        
+print(sum)
+
+#Ans:32
+
+list=[[2,5,7,8],[2,3,4,5],[4,3,2,1]]
+sum=0
+for i in range(0,3):
+    for j in range(0,3):
+        if list[i][j]==2 or list[i][j]==3:
+            sum=sum+list[i][j]
+
+print(sum)
+
+#Ans:12
