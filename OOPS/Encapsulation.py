e8342635b0bff9c700fc2238940b180a628e44e5
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-21 16:46:01 +0530

    Update Encapsulation.py

--- OOPS/Encapsulation.py
+++ OOPS/Encapsulation.py
@@ -34,6 +34,37 @@ obj.add()
 obj.__name # cannot call private
 
 #--------------------------------------------------------------
+#Abstract base class
+import abc
+class AbstractAnimal(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def walk(self):
+        ''' data '''
+
+    @abc.abstractmethod
+    def talk(self):
+        ''' data '''
+
+class Duck(AbstractAnimal):
+    name = ''
+
+    def __init__(self, name):
+        print('duck created.')
+        self.name = name
+
+    def walk(self):
+        print('walks')
+
+    def talk(self):
+        print('quack')
+
+obj = Duck('duck1')
+obj.talk()
+obj.walk()
+
+
 
 
 
