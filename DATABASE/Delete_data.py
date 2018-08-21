e31a0245833323337f7353f833801d385cffb89a
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-21 19:51:59 +0530

    Update Delete_data.py

--- DATABASE/Delete_data.py
+++ DATABASE/Delete_data.py
@@ -1,14 +1,15 @@
-import pymysql;
-db = pymysql.connect("localhost","root","","anbu");
-cursor = db.cursor();
-#sql = "create table excel(name varchar(30))";
-sql=  "delete from ex3 where user='root'";
-try:
-   cursor.execute(sql);
-   db.commit();
-   print("Sucess");
-except:
-    db.rollback();
-    print("Check");
-db.close();
-----------------------------------------------------
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="pythondb"
+)
+mycursor = mydb.cursor()
+sql = "delete from details where name='guhan'"
+mycursor.execute(sql)
+mydb.commit()
+print(mycursor.rowcount, "record(s) deleted")
+
+Result:
+   1 record(s) deleted
