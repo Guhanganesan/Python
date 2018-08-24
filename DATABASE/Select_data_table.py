f446a9d37b4bc8b95eaefc3aa29b67d7aaf460a5
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-24 16:53:17 +0530

    Update Select_data_table.py

--- DATABASE/Select_data_table.py
+++ DATABASE/Select_data_table.py
@@ -1,17 +1,19 @@
-	import pymysql;
-	db = pymysql.connect("localhost","root","","anbu");
-	cursor = db.cursor();
-	sql=  "select * from ex3 where mob='9798'";
-	try:
-	   cursor.execute(sql);
-	   results=cursor.fetchall();
-	   for row in results:
-       name=row[0];
-       user=row[1];
-       pass1=row[2];
-       mob=row[3];
-       print(name,user,pass1,mob);
-	except:
-       print("Error");
-	db.close();
------------------------------------------------------
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="abi"
+)
+mycursor = mydb.cursor()
+sql=  "select * from details where mobile='8757851'"
+try:
+  mycursor.execute(sql)
+  results=mycursor.fetchall()
+  for row in results:
+       name=row[0]
+       age=row[1]
+       mobile=row[2]
+       print(name,age,mobile)
+except:
+       print("Error")
