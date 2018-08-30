959be88573f4bf0b0916347718da8225a1fce4d2
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-08-30 07:21:48 +0530

    Update Dictionary_Intro.py

--- Dictionary_Intro.py
+++ Dictionary_Intro.py
@@ -1,3 +1,7 @@
+Python dictionary is an unordered collection of items. 
+While other compound data types have only value as an element, a dictionary has a key: value pair.
+Dictionaries are optimized to retrieve values when the key is known.
+
 #create dictionary
 details ={
            'name':"Guhan",
