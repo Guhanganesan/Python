0335ae2ace8df52b21ce061fda5da030e85d046c
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-03 07:32:25 +0530

    Update Set_Intro.py

--- Set_Intro.py
+++ Set_Intro.py
@@ -1,13 +1,23 @@
-#Python Set is an unordered collection of unique elements
-set1 = {1, 2, 3, 4, 2, 3, 1}
+set1 = {1, 3,2,5,4,3, 1}
 print(set1)
+
+set2={'d','b','a','d','c','a','f'}
+print(set2)
 
 set2 = {1, 2, 3, (1, 2, 3), 2.45, "Python", 2, 3}
 print(set2)
+#(1,2,3) cannot modify the tuple
 
+#List of duplicate elements
 theList = [1, 2, 3, 4, 2, 3, 1]
 theSet = set(theList)
 print(theSet)
+
+#cannot modify the tuple
+tuple = (1, 2, 3, 4, 2, 3, 1)
+theSet = set(tuple)
+print(tuple)
+print("-----------------------------------")
 
 theSet = set()
 theSet.add(1)
@@ -20,54 +30,22 @@ print(theSet)
 theSet.update([5,7])
 print(theSet)
 
-# Remove Element
-theSet.discard(3)
-print(theSet)
-
+#Remove
 theSet.remove(5)
 print(theSet)
 
-#Python set operations
-
-A={1,2,3,4}
-B={2,3,5,7}
-    #Union-all unique elements of two sets.
-print(A.union(B))
-    #Intersetion - common unique elements of two sets.
-print(A.intersection(B))
-    # Difference -set containing items from set A which are not common in set B
-print(A.difference(B))
-print(B.difference(A))
-
-A={1,2,3,4}
-B={2,4}
-
-print(A.issubset(B))
-print(B.issubset(A))
-
-A={1,2,3,4}
-B={5,6}
-print(A.isdisjoint(B))
-
-m={3,5,4,2,7}
-n=m.pop()
-print(n)
 
 
-Result:
-
-{1, 2, 3, 4}
+Result:-
+    
+{1, 2, 3, 4, 5}
+{'c', 'd', 'a', 'b', 'f'}
 {1, 2, 3, 2.45, 'Python', (1, 2, 3)}
 {1, 2, 3, 4}
+(1, 2, 3, 4, 2, 3, 1)
+-----------------------------------
 {1, 2, 3}
 {1, 2, 3, 5, 7}
-{1, 2, 5, 7}
-{1, 2, 7}
-{1, 2, 3, 4, 5, 7}
-{2, 3}
-{1, 4}
-{5, 7}
-False
-True
-True
-2
+{1, 2, 3, 7}
+
+
