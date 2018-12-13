afb46bdc400859ca9fb313ce03ea608f7be87348
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-12-13 13:33:36 +0530

    Rename 0.py to s0.py

---
+++ Basic_operators/s0.py
@@ -0,0 +1,45 @@
+x=10
+y=True #1
+print(x+y) #11
+
+print(10+False)
+
+print(True+True)
+
+print(True-False)
+
+print(1.2+True)
+
+print(type(True))
+
+print(int(1.5))
+
+print(bool(1.5))
+
+print(int(True))
+
+print(bool(-3.3))
+
+Result:-
+
+10
+2
+1
+2.2
+<class 'bool'>
+1
+True
+1
+True
+
+D:\py\Basics\Exception>python 8.py
+11
+10
+2
+1
+2.2
+<class 'bool'>
+1
+True
+1
+True
